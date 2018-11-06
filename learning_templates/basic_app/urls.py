3fa1191294f452024565cced4e4a5332c86c0061
yeongho.shin <yeongho.shin@gmail.com>
2018-11-06 15:01:09 +0900

    example project

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from basic_app import views
+
+
+# TEMPLATE TAGGING
+app_name = 'basic_app'  # Django recognizes 'app_name'
+
+
+urlpatterns = [
+    path('relative/', views.relative, name='relative'),
+    path('other/', views.other, name='other'),
+]
---
+++ learning_templates/basic_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {'text': 'hello world', 'number': 100}
+    return render(request, 'basic_app/index.html', context)
+
+
+def other(request):
+    return render(request, 'basic_app/other.html')
+
+
+def relative(request):
+    return render(request, 'basic_app/relative_url_templates.html')
+
